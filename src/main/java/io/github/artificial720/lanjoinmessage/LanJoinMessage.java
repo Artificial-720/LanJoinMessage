0db1bf950d39ba1c4e3cc044167bfceb14858282
Artificial-720 <76878013+Artificial-720@users.noreply.github.com>
2024-11-29 14:39:01 -0600

    Finished v1 of the plugin

--- src/main/java/io/github/artificial720/lanjoinmessage/LanJoinMessage.java
+++ src/main/java/io/github/artificial720/lanjoinmessage/LanJoinMessage.java
@@ -1,17 +1,19 @@
 package io.github.artificial720.lanjoinmessage;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LanJoinMessage extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        saveDefaultConfig();
+        Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        getLogger().info("The Lan Join Message Plugin has been enabled");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("The Lan Join Message Plugin has been disabled");
     }
 }
--- src/main/java/io/github/artificial720/lanjoinmessage/PlayerJoinListener.java
+++ src/main/java/io/github/artificial720/lanjoinmessage/PlayerJoinListener.java
@@ -1,4 +1,37 @@
 package io.github.artificial720.lanjoinmessage;
 
-public class PlayerJoinListener {
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.net.InetAddress;
+
+public class PlayerJoinListener implements Listener {
+    private final LanJoinMessage plugin;
+
+    public PlayerJoinListener(LanJoinMessage plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        InetAddress playerAddress = player.getAddress().getAddress();
+
+        // Check if player is connected from LAN IP address
+        if (playerAddress != null && playerAddress.isSiteLocalAddress()) {
+            FileConfiguration config = plugin.getConfig();
+            String rawMessage = config.getString("lan-message");
+
+            MiniMessage miniMessage = MiniMessage.miniMessage();
+            if (rawMessage != null) {
+                Component message = miniMessage.deserialize(rawMessage);
+                player.sendMessage(message);
+            }
+        }
+    }
 }
